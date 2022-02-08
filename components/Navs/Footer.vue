680c10db9198fbfecf3d3e8a1f27729c2c527e85
Ryuze <alfianyunianto74@gmail.com>
2022-02-08 19:00:22 +0700

    minor update at footer

--- components/Navs/Footer.vue
+++ components/Navs/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="block bottom-0 left-0 right-0 bg-[#FCDE67] py-5 text-gray-800">
     <section class="grid place-items-end container mx-auto">
-      <a href="https://github.com/Ryuze">
+      <a href="https://github.com/Ryuze" target="_blank">
         <p class="font-semibold font-head mr-2">Alfian Yunianto Suseno</p>
       </a>
     </section>
